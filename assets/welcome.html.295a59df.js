38d51c7ff398511b1c85ba934e5d35be0776af65
daocab <Marlon.Asante40593@mail.ru>
2022-04-07 18:24:02 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.295a59df.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to DAO.Cab",lang:"en-US",frontmatter:{index:1,title:"Welcome to DAO.Cab",author:"DAO.Cab",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`DAO Cab : Autonomous Community Management.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://dao.cab/welcome.html"}],["meta",{property:"og:title",content:"Welcome to DAO.Cab"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"DAO.Cab"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>DAO Cab : Autonomous Community Management.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"DAO Cab",slug:"dao-cab",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why DAOCab?",slug:"why-daocab",children:[]},{level:2,title:"Tech Stack to Build DAO Cab",slug:"tech-stack-to-build-dao-cab",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649327011e3,updatedTime:1649327011e3,contributors:[{name:"daocab",email:"Marlon.Asante40593@mail.ru",commits:1}]},readingTime:{minutes:.44,words:133},filePathRelative:"welcome.md"};export{e as data};
